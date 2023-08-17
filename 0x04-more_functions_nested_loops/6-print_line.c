8a08b7ff4180765433efac947a1a4dd8a2b92b2f
abdulwahid munewer <abdu12.aau@gmail.com>
2023-08-17 13:03:24 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
@@ -21,4 +23,3 @@ void print_line(int n)
 	}
 
 }
-
